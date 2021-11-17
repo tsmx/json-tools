9b389a51cde278c23e1c18ac7cc16e26db5fa938
tsmx <strs@freenet.de>
2021-11-17 21:29:59 +0100

    First set of unit-tests

--- test/json-traverse-tools.test.js
+++ test/json-traverse-tools.test.js
@@ -8,4 +8,22 @@ describe('json-traverse-tools test suite', () => {
         expect(jtt.getDepth(obj)).toBe(0);
     });
 
+    it('tests getting the depth of a nested JSON object with depth 1', async () => {
+        let obj = require('./objects/nested-1.json');
+        expect(jtt.getDepth(obj)).toBe(1);
+    });
+
+    it('tests string obfuscation with standard parameters', async () => {
+        let obj = require('./objects/simple.json');
+        expect(obj.firstName).toStrictEqual('Dow')
+        expect(obj.lastName).toStrictEqual('Jones');
+        expect(obj.age).toBe(30);
+        expect(obj.city).toStrictEqual('New York');
+        jtt.obfuscateStrings(obj);
+        expect(obj.firstName).toStrictEqual('Dow')
+        expect(obj.lastName).toStrictEqual('Jon***');
+        expect(obj.age).toBe(30);
+        expect(obj.city).toStrictEqual('New*****');
+    });
+
 });
